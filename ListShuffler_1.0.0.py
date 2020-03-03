9854093316817ffae1002a042913a3f64189a707
djccnt15 <53306911+djccnt15@users.noreply.github.com>
2020-03-03 19:32:28 +0900

    Update ListShuffler_1.0.0.py

--- ListShuffler_1.0.0.py
+++ ListShuffler_1.0.0.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
-prt1 = '''파일 이름 입력:
-(확장자는 xlsx입니다. 확장자를 빼고 입력하세요.)'''
+prt = '''입력할 목록 양식은 samplelist.xlsx 파일을 참고해주세요.
+목록은 xlsx 파일 이어야 합니다. 확장자를 빼고 입력하세요.
+파일 이름 입력:'''
 
 def shuffle():
     result_df = target_df.sample(frac=1).reset_index(drop=True)
@@ -9,7 +10,7 @@ def shuffle():
     result_df.to_excel('%s_랜덤순서.xlsx' %(target), index=False)
     return print(result_df)
 
-print(prt1)
+print(prt)
 target = str(input())
 target_df = pd.read_excel('%s.xlsx' %(target))
 # print(target_df)
